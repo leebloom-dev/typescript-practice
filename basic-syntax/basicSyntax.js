50e32b02230f084a9c5e60e2d79e1b2981415680
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-18 15:02:37 -0400

    update greeting()

--- basic-syntax/basicSyntax.js
+++ basic-syntax/basicSyntax.js
@@ -1,38 +1,35 @@
 /**
  * TYPESCRIPT AND OBJECT ORIENTATION
- * 
+ *
  * Object
  *  - real time representation of any entity.
- * 
+ *
  * State
  *  - object attributes description
- * 
+ *
  * Behavior
  *  - how an object acts
- * 
+ *
  * Identity
  *  - a unique value distinguishes an object from other objecs.
- * 
+ *
  *  Class
  *  - OOP bluepring for creating objects
  *  - encapsutes date for the object
- * 
+ *
  * Method
  *  - communicates between objects.
  */
-
-class Greeting {
-
-    greet(): void {
-        console.log(
-            "This greeting method 'greeting()' is " +
+var Greeting = /** @class */ (function () {
+    function Greeting() {
+    }
+    Greeting.prototype.greet = function () {
+        console.log("This greeting method 'greeting()' is " +
             "in the class Greeting.\n" +
             "Don't forget to compile your TypeScript using 'tsc fileName.ts'\n" +
-            "for every change your make!"
-        );
-    }
-
-}
-
-let someObject = new Greeting();
+            "for every change your make!");
+    };
+    return Greeting;
+}());
+var someObject = new Greeting();
 someObject.greet();
